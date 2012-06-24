b714584fe28e320b2295830e865e250aa4e5540d
Mark Lee <mark.lee@capricasoftware.co.uk>
2012-06-24 20:18:31 +0100

    Initial port of support classes for the native log from vlcj.

---
+++ Caprica/VlcSharp/Log/LogEventListener.cs
@@ -0,0 +1,39 @@
+/*
+ * This file is part of VLCSHARP.
+ *
+ * VLCSHARP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VLCSHARP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VLCSHARP.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Copyright 2012 Caprica Software Limited.
+ */
+
+using System;
+
+using Caprica.VlcSharp.Binding.Internal;
+
+namespace Caprica.VlcSharp.Log {
+
+    /**
+     * Specification for a component that will receive native log messages.
+     */
+    public interface LogEventListener {
+    
+        /**
+         * Process a native log message.
+         * 
+         * @param level log level
+         * @param message log message
+         */
+        void log(libvlc_log_level_e level, string message);
+    }
+}
