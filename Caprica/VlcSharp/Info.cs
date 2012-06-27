e88274ac775c3a5c05073c6add68d6ebd4bea952
Mark Lee <mark.lee@capricasoftware.co.uk>
2012-06-27 18:00:00 +0100

    Be consistent with case.

--- Caprica/VlcSharp/Info.cs
+++ Caprica/VlcSharp/Info.cs
@@ -44,18 +44,18 @@ namespace Caprica.VlcSharp {
          * Application license (GPL3) summary text.
          */
         private static readonly string LICENSE_MSG =
-            "vlcsharp is free software: you can redistribute it and/or modify"     + "\n" +
+            "VLCSHARP is free software: you can redistribute it and/or modify"     + "\n" +
             "it under the terms of the GNU General Public License as published by" + "\n" +
             "the Free Software Foundation, either version 3 of the License, or"    + "\n" +
-            "(at your option) any later version."                                  + "\n" +    
+            "(at your option) any later version."                                  + "\n" +
             ""                                                                     + "\n" +
-            "vlcsharp is distributed in the hope that it will be useful,"          + "\n" +
+            "VLCSHARP is distributed in the hope that it will be useful,"          + "\n" +
             "but WITHOUT ANY WARRANTY; without even the implied warranty of"       + "\n" +
             "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"        + "\n" +
             "GNU General Public License for more details."                         + "\n" +
             ""                                                                     + "\n" +
             "You should have received a copy of the GNU General Public License"    + "\n" +
-            "along with vlcsharp.  If not, see <http://www.gnu.org/licenses/>."    + "\n" +
+            "along with VLCSHARP.  If not, see <http://www.gnu.org/licenses/>."    + "\n" +
             ""                                                                     + "\n" +
             "Copyright 2012 Caprica Software Limited."                             + "\n";
           
