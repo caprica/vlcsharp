30c74368fa75f0733580fcafd261164ef28ed5c7
Mark Lee <mark.lee@capricasoftware.co.uk>
2012-06-27 18:01:05 +0100

    Added assembly info.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,31 @@
+/*
+ * This file is part of VLCSHARP.
+ *
+ * VLCSHARP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VLCSHARP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VLCSHARP.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Copyright 2012 Caprica Software Limited.
+ */
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyDescription("Platform independent C# framework for the vlc media player.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Caprica Software Limited")]
+[assembly: AssemblyCopyright("(C)2012 Caprica Software Limited")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyProduct("vlcsharp")]
+[assembly: AssemblyTitle("vlcsharp")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("0.1.0.*")]
